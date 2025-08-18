cd3107d496d0da485bcd28b0d1ae20b75c787081
leticia soares do nascimento de camello barros <lsncb@cesar.school>
2025-08-18 20:22:11 -0300

    Update primos.py
    ocultar print desnecessário e explicar o código

--- primos.py
+++ primos.py
@@ -4,8 +4,9 @@ for i in range(1, numero + 1):
     if numero % i == 0:
         lista_de_divisores.append(i)
 
-print(lista_de_divisores)
+# print(lista_de_divisores) // etapa que serve meramente para verificar se as et
 if len(lista_de_divisores) <= 2:
+    # nesse caso o número só será divisível por um e por ele mesmo 
     print("esse numero é primo ")
 else:
     print("esse número não é primo ")
